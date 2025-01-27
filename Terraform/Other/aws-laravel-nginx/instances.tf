9f333eca93e8bf5371c378532d1fdc7a57f31813
Davidnbr <davidnbr_98@hotmail.com>
2025-01-26 20:26:18 -0500

    Add: Instance creation

--- Terraform/Other/aws-laravel-nginx/instances.tf
+++ Terraform/Other/aws-laravel-nginx/instances.tf
@@ -34,3 +34,38 @@ resource "aws_security_group" "sec_grp_ec2_allow_ssh_http" {
     Name = "sec_grp_ec2_allow_ssh_http"
   }
 }
+
+# AMI data source
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+
+}
+
+# Create instance
+resource "aws_instance" "ec2_laravel" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  key_name                    = aws_key_pair.tf_key.key_name
+  vpc_security_group_ids      = [aws_security_group.sec_grp_ec2_allow_ssh_http.id]
+  associate_public_ip_address = true
+
+  subnet_id = aws_subnet.subnet_public_laravel["10.0.1.0/26"].id
+
+  tags = {
+    Name = "ec2_laravel"
+  }
+
+  depends_on = [aws_security_group.sec_grp_ec2_allow_ssh_http]
+
+}
