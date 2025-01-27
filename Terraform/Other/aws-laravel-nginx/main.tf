fa8c4279aee37e42e333a504b432d7fc5ecf93db
Davidnbr <davidnbr_98@hotmail.com>
2025-01-26 19:16:57 -0500

    Add: VPC creation

--- Terraform/Other/aws-laravel-nginx/main.tf
+++ Terraform/Other/aws-laravel-nginx/main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+# VPC configuration
+resource "aws_vpc" "vpc_laravel" {
+  cidr_block           = "10.0.1.0/24"
+  instance_tenancy     = "default"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "vpc_laravel"
+  }
+}
