3689b013d9411a06e32f9540862700716f42dc87
Davidnbr <davidnbr_98@hotmail.com>
2025-01-26 20:17:13 -0500

    Add: Security group for EC2

--- Terraform/Other/aws-laravel-nginx/instances.tf
+++ Terraform/Other/aws-laravel-nginx/instances.tf
@@ -1,1 +1,36 @@
 ### EC2 instances
+# Security group
+resource "aws_security_group" "sec_grp_ec2_allow_ssh_http" {
+  name        = "sec_grp_ec2_allow_ssh_http"
+  description = "Allow SSH and HTTP inbound traffic"
+  vpc_id      = aws_vpc.vpc_laravel.id
+
+  # SSH port
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP port
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags = {
+    Name = "sec_grp_ec2_allow_ssh_http"
+  }
+}
