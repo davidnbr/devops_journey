101ec45524c45ff8434d961560e7267d9792635d
Davidnbr <davidnbr_98@hotmail.com>
2025-02-11 13:58:44 -0500

    Edit: Cloud organization declaration in TF tutorial

--- Terraform/learn-terraform-aws-instance/main.tf
+++ Terraform/learn-terraform-aws-instance/main.tf
@@ -1,29 +1,25 @@
 terraform {
-    cloud {
-        organization = "Devops_Journey_DB"
-        workspaces {
-            name = "learn-terraform-aws"
-        }
-    }
+  cloud {
+  }
 
-    required_providers {
-        aws = {
-        source  = "hashicorp/aws"
-        version = "~> 4.16"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
+  }
 
-    required_version = ">= 1.2.0"
+  required_version = ">= 1.2.0"
 }
 
 # Deploy made with HCP so ENV variables are specified there
 provider "aws" {
-    #shared_config_files = ["$HOME/.aws/config"]
-    #profile = "admin-1"
-    region  = "us-west-2"
+  #shared_config_files = ["$HOME/.aws/config"]
+  #profile = "admin-1"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
-    ami           = "ami-08d70e59c07c61a3a"
-    instance_type = "t2.micro"
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
 }
