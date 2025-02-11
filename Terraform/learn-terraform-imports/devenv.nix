d6ed869cbd70c820515964f654225eda079ab8eb
Davidnbr <davidnbr_98@hotmail.com>
2025-02-11 11:04:39 -0500

    Edit: .gitignore Add: Devenv variables and configuration Add: Direnv configuration

---
+++ Terraform/learn-terraform-imports/devenv.nix
@@ -0,0 +1,36 @@
+{ inputs, pkgs, ... }:
+
+{
+  git-hooks.hooks = {
+    # lint shell scripts
+    shellcheck.enable = true;
+    # execute example shell from Markdown files
+    mdsh.enable = true;
+    # lint GitHub Actions workflows
+    actionlint.enable = true;
+    # lint Ansible
+    ansible-lint.enable = true;
+    # lint Terraform
+    tflint.enable = true;
+  };
+
+    # Env vars
+    dotenv.enable = true;
+
+    # Tools
+    packages = [
+    pkgs.aws-vault
+    pkgs.trivy
+    ];
+
+
+    # Languages used
+    # Ansible
+    languages.ansible.enable = true;
+
+    # Terraform
+    languages.terraform = {
+      enable = true;
+      version = "1.10.5";
+    };
+}
